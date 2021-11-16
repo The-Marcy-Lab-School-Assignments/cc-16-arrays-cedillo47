c100cb49400f1cb5b3630987a9b3df35198c9a8f
leo <cedillo47@gmail.com>
2021-11-16 16:31:01 +0000

    compleate

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+function replaceWithYerr(arr,start = 0, end = arr.length -1 ){
+let newArr = [];
+for(let i = 0; i < arr.length ; i++){
+  if (i >= start && i < start + end ){
+    newArr.push('yer')  
+   } else{
+       newArr.push(arr[i])
+   }
+  } 
+  return newArr
+ }
+ 
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+console.log(replaceWithYerr(months,2,2))
